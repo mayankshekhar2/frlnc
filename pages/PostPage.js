17308597fba72f56e9f30948f984d49c6da5bdd7
Mayank <veershekhar743@gmail.com>
2018-04-08 23:30:58 +0530

    new

--- pages/PostPage.js
+++ pages/PostPage.js
@@ -77,7 +77,142 @@ var postPage = {
 
     validateErrorPage : function(browser) {
         this.waitForElementVisible('@errorLink',this.timeout);
-    }
+    },
+
+    ValidateFbCountsForPost : function (browser){
+        var sel1 = pageElements.headerFacebookCounter.selector;
+        var sel2 = pageElements.footerFacebookCounter.selector;
+        this.validateFbCount(browser,sel1,sel2);
+    },
+
+    ValidateTwitterCountsForPost : function (browser){
+        var sel1 = pageElements.headerTwitterCounter.selector;
+        var sel2 = pageElements.footerTwitterCounter.selector;
+        this.validateTwitterCount(browser,sel1,sel2);
+    },
+
+    ValidatePlusCountsForPost : function (browser){
+        var sel1 = pageElements.headerPlusCounter.selector;
+        var sel2 = pageElements.footerPlusCounter.selector;
+        this.validatePlusCount(browser,sel1,sel2);
+    },
+
+    validateFbCount : function (browser,sel,sel2) {
+        
+        var pos;
+        var UIHeadercount;
+        var UIFootercount;
+        var CookieCount;
+        var url;
+        var cookieJSON;
+
+        browser.useXpath()
+        .pause(2000)
+
+        this.getAttribute(sel,'data-url',function(result){
+            url = result.value.toString();
+            console.log("data-url of Article= "+url);
+        });
+        
+        this.waitForElementVisible(sel, this.timeout);
+        this.getText(sel,function(result){
+            console.log("Facebook UI Header Count="+result.value);
+            UIHeadercount = result.value;
+        });  
+
+        this.getText(sel2,function(result){
+            console.log("Facebook UI Footer Count="+result.value);
+            UIFootercount = result.value;
+        });        
+
+        browser.pause(2000)
+        .getCookie('socialCounter',function(result){
+            cookieJSON = utils.getJSONObj(result.value.toString());
+            pos = utils.getBlogPositioninJSON(url,cookieJSON);
+            CookieCount = cookieJSON[pos].facebook;
+            console.log("Facebook cookie Count="+CookieCount);
+            chai.assert.equal(CookieCount,UIHeadercount,"Fb Count does not match with cookie");
+            chai.assert.equal(UIHeadercount,UIFootercount,"Fb counts do not match between header and footer.")
+        });
+    },
+
+    validateTwitterCount : function (browser,sel,sel2) {
+        
+        var pos;
+        var UIHeadercount;
+        var UIFootercount;
+        var CookieCount;
+        var url;
+        var cookieJSON;
+
+        browser.useXpath()
+        .pause(2000)
+
+        this.getAttribute(sel,'data-url',function(result){
+            url = result.value.toString();
+            console.log("data-url of Article= "+url);
+        });
+        
+        this.waitForElementVisible(sel, this.timeout);
+        this.getText(sel,function(result){
+            console.log("Twitter UI Header Count="+result.value);
+            UIHeadercount = result.value;
+        });  
+
+        this.getText(sel2,function(result){
+            console.log("Twitter UI Footer Count="+result.value);
+            UIFootercount = result.value;
+        });        
+
+        browser.pause(2000)
+        .getCookie('socialCounter',function(result){
+            cookieJSON = utils.getJSONObj(result.value.toString());
+            pos = utils.getBlogPositioninJSON(url,cookieJSON);
+            CookieCount = cookieJSON[pos].twitter;
+            console.log("Twitter cookie Count="+CookieCount);
+            chai.assert.equal(CookieCount,UIHeadercount,"Twitter Count does not match with cookie");
+            chai.assert.equal(UIHeadercount,UIFootercount,"Twitter counts do not match between header and footer.")
+        });
+    },
+
+    validatePlusCount : function (browser,sel,sel2) {
+        
+        var pos;
+        var UIHeadercount;
+        var UIFootercount;
+        var CookieCount;
+        var url;
+        var cookieJSON;
+
+        browser.useXpath()
+        .pause(2000)
+
+        this.getAttribute(sel,'data-url',function(result){
+            url = result.value.toString();
+            console.log("data-url of Article= "+url);
+        });
+        
+        this.waitForElementVisible(sel, this.timeout);
+        this.getText(sel,function(result){
+            console.log("Google Plus UI Header Count="+result.value);
+            UIHeadercount = result.value;
+        });  
+
+        this.getText(sel2,function(result){
+            console.log("Google Plus UI Footer Count="+result.value);
+            UIFootercount = result.value;
+        });        
+
+        browser.pause(2000)
+        .getCookie('socialCounter',function(result){
+            cookieJSON = utils.getJSONObj(result.value.toString());
+            pos = utils.getBlogPositioninJSON(url,cookieJSON);
+            CookieCount = cookieJSON[pos].plus;
+            console.log("Google Plus cookie Count="+CookieCount);
+            chai.assert.equal(CookieCount,UIHeadercount,"Plus Count does not match with cookie");
+            chai.assert.equal(UIHeadercount,UIFootercount,"Plus counts do not match between header and footer.")
+        });
+    },
 };
 
 module.exports = {
